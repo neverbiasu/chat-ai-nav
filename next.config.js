36c9b57f1d59617e2aa307ec81130fefaa06bdea
neverbiasu <1751162157@qq.com>
2025-04-07 18:49:59 +0800

    feat: Add ESLint and Prettier config

--- next.config.js
+++ next.config.js
@@ -1,23 +1,24 @@
+/* eslint-env node */
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   images: {
     domains: [
-      "cdn.oaistatic.com",
-      "cdn.midjourney.com",
-      "claude.ai",
-      "github.githubassets.com",
-      "www.jasper.ai",
-      "lh3.googleusercontent.com",
-      "perplexity.ai",
-      "framerusercontent.com",
-      "github.blog",
-      "www.gstatic.com",
-      "is1-ssl.mzstatic.com",
-      "logos-world.net",
-      "cdn-icons-png.flaticon.com",
-    ],
-  },
-};
+      'cdn.oaistatic.com',
+      'cdn.midjourney.com',
+      'claude.ai',
+      'github.githubassets.com',
+      'www.jasper.ai',
+      'lh3.googleusercontent.com',
+      'perplexity.ai',
+      'framerusercontent.com',
+      'github.blog',
+      'www.gstatic.com',
+      'is1-ssl.mzstatic.com',
+      'logos-world.net',
+      'cdn-icons-png.flaticon.com'
+    ]
+  }
+}
 
-module.exports = nextConfig;
+module.exports = nextConfig
